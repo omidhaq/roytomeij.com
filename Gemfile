204a1a34bb4a987b151299c5e4b9977c96a925cd
Roy Tomeij <roy@tomeij.net>
2012-01-10 22:31:55 +0100

    Switch to Nesta: add new files

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'nesta', '0.9.11'
+gem 'compass', '~> 0.11.6'
+
+# gem (RUBY_VERSION =~ /^1.9/) ? 'ruby-debug19': 'ruby-debug'
---
+++ config.rb
@@ -0,0 +1,21 @@
+# HTTP paths
+http_path = '/'
+http_stylesheets_path = '/css'
+http_images_path = '/images'
+http_javascripts_path = '/javascripts'
+
+# File system locations
+sass_dir = 'views/sass'
+css_dir = 'public/css'
+images_dir = 'public/images'
+javascripts_dir = 'public/javascripts'
+
+# Set to true for easier debugging
+line_comments = false
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style = :expanded
+
+# Determine whether Compass asset helper functions generate relative
+# or absolute paths
+relative_assets = true
